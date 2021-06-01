498ecd290da2aa204f6aadc43d4825e5bb88861e
UlisesSerrano <62449335+UlisesSerrano@users.noreply.github.com>
2021-06-01 08:49:17 -0500

    Added some prints of errors

--- virtual_machine.py
+++ virtual_machine.py
@@ -200,6 +200,7 @@ def run(instruction_pointer=0):
                 instruction_pointer += 1
             else:
                 print('ERROR: Invalid input type', value, third_element)
+                program.display_output(f'ERROR: Invalid input type: {value}')
         return instruction_pointer
 
     def ireturn():
@@ -218,6 +219,7 @@ def run(instruction_pointer=0):
             instruction_pointer += 1
         else:
             print('ERROR: Index out of bounds', value)
+            program.display_output(f'ERROR: Index out of bounds: {value}')
         return instruction_pointer
 
     def iassign():
@@ -240,6 +242,7 @@ def run(instruction_pointer=0):
 
     def instruction_error():
         print('ERROR: Wrong instruction')
+        program.display_output(f'ERROR: Wrong instruction')
 
     instruction_switch = {
         'goto': igoto,
